af399add440b88f5d45882ba0c97cec8e989b55e
Trevor <holl.10@osu.edu>
2013-02-02 12:39:07 -0500

    Added missing js file and delete project button
    Delete project button/confirmation is on the main Projects page, not the
    project details page

---
+++ Resources/javascript/validateForm.js
@@ -0,0 +1,10 @@
+function verifyForm(form) 
+{
+	var e = form.elements;
+	if(e.projectTitle.value == "")
+	{
+		alert('Project title must be filled in.');
+		e.projectTitle.focus();
+		return false;
+	}
+}
