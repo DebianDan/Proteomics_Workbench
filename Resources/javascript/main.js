1af1fce1731c975fbdb94efeb1827e5d6cd46585
DebianDan <tucholski.4@osu.edu>
2013-02-03 17:49:45 -0500

    DB is fixed, now ADD Project and LIST Projects is working correctly
    There were a couple problems I solved.  First off the reason why append
    wasn't working was we were adding the script at the top and the ccs
    selector wasn't finding the list.  Therefore it was adding to nowhere. I
    solved that by including the script main.js at the bottom of the page.
    Also you were closing the ResultSet from the DB before you read it and
    that was releasing the memory and the results.

--- Resources/javascript/main.js
+++ Resources/javascript/main.js
@@ -1,29 +1,3 @@
-//should be wrapped into a listProjects() function. proof of concept right now
-//should be able to pass each 'row' into the constructor to create a new pw.projects.project object. or the row collection to create a project collection
-$('#projects').live('pagebeforecreate', function(event){
-    // manipulate this page before its widgets are auto-initialized
-    /*if(projects = pw.projects.getProject()){
-     var pList = $("#projectList");
-     while(projects.isValidRow()) {
-     $("#projectList").append("<li>"+ projects.fieldByName('name') +"</li>");
-     projects.next();
-     }
-     pList.listview('refresh'); //tell jquery mobile that we added some stuff so it can style it
-     }else{
-     alert("no projects found.");
-     }*/
-    alert('test');
-});
-
-/////////////
-//get handle to the database
-var db = Ti.Database.openFile(Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'capDB.sqlite'));
-//Create a table
-db.execute("CREATE  TABLE  IF NOT EXISTS 'main'.'projects' ('pid' INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , 'name' VARCHAR NOT NULL , 'description' VARCHAR, 'active' BOOL NOT NULL  DEFAULT 1, 'date_created' DATETIME NOT NULL DEFAULT CURRENT_DATE )");
-//try to insert a row into the DB
-db.execute("INSERT INTO projects (name, description, active, date_created) VALUES('First Project!', 'This is the first project to see if it gets inserted correctly...', 1, DATETIME('NOW'))");
-/////////////
-
 //define our proteomics workbench namespace to hold all of our stuff
 var pw = {};
 pw.projects = {
@@ -36,19 +10,23 @@ pw.projects = {
     //returns Ti.Database.ResultSet
     getProject : function(id){
         var results = {};
-        //Select from Table
-        var rows = db.execute("SELECT pid, name, description, active FROM projects");
+        //Select Projects in order of Newest First
+        var rows = db.execute("SELECT pid, name, description, active FROM projects ORDER BY pid DESC");
         results = rows;
-        //Release memory once you are done with the resultset and the database
-        rows.close();
-        db.close();
         return results;
-
-
-
     }
 };
 
+/////////////
+//get handle to the database
+var db = Ti.Database.openFile(Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'capDB.sqlite'));
+//Create a table
+db.execute("CREATE  TABLE  IF NOT EXISTS 'main'.'projects' ('pid' INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , 'name' VARCHAR NOT NULL , 'description' VARCHAR, 'active' BOOL NOT NULL  DEFAULT 1, 'date_created' DATETIME NOT NULL DEFAULT CURRENT_DATE )");
+//Propogate the list of projects from the db
+listProjects();
+/////////////
+
+//Create Project Functionality (needs to be cleaned up)
 function processForm(form) 
 {
 	var e = form.elements;
@@ -60,7 +38,32 @@ function processForm(form)
 	}
 	else
 	{
-		alert('Title: ' + e.projectTitle.value);
-		alert('Discription: ' + e.projectDescription.value);
+		//create the new project
+		db.execute("INSERT INTO projects (name, description, active, date_created) VALUES('" +e.projectTitle.value+ "', '" +e.projectDescription.value +"', 1, DATETIME('NOW'))");
+		
+		//TODO should check if the add was successful
+		alert(e.projectTitle.value + " was added successfully!");
+		
+		//TODO should take you to that project details page of the newly added project 
 	}
+}
+
+//should be wrapped into a listProjects() function. proof of concept right now
+//should be able to pass each 'row' into the constructor to create a new pw.projects.project object. or the row collection to create a project collection
+function listProjects() 
+{
+//$('#projects').live('pagebeforecreate', function(event){
+    // manipulate this page before its widgets are auto-initialized
+	if(projects = pw.projects.getProject()){
+		var pList = $("#projectList");
+		while(projects.isValidRow()) {
+			 $("#projectList").append("<li><a href=\"#projectDetails\">"+ projects.fieldByName('name') +"</li>");
+			 projects.next();
+		}
+		//TODO don't think this is needed...
+		//pList.listview('refresh'); //tell jquery mobile that we added some stuff so it can style it
+     }else{
+		alert("no projects found.");
+     }
+	 
 }
