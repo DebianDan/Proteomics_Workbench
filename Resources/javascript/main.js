69878160117f14f5711abb141bae4a359eed6a7c
Trevor <holl.10@osu.edu>
2013-02-03 15:54:58 -0500

    Moved some js and added some Create Project tests

--- Resources/javascript/main.js
+++ Resources/javascript/main.js
@@ -48,3 +48,19 @@ pw.projects = {
 
     }
 };
+
+function processForm(form) 
+{
+	var e = form.elements;
+	if(e.projectTitle.value == "")
+	{
+		alert('Project title must be filled in.');
+		e.projectTitle.focus();
+		return false;
+	}
+	else
+	{
+		alert('Title: ' + e.projectTitle.value);
+		alert('Discription: ' + e.projectDescription.value);
+	}
+}
