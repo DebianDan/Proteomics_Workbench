ca854ab3c5b9bf0536cbed9bf273e2970030d805
steve <steveruiz@gmail.com>
2013-03-17 20:45:26 -0400

    project details page is now rendering assets almost correctly. needs to be made fully templated.

--- Resources/javascript/pw.projects.js
+++ Resources/javascript/pw.projects.js
@@ -1,5 +1,6 @@
 //PROJECTS STUFF
-//TODO: pw.projects.getProjectEx({id:1,success:function(p){console.log(JSON.stringify(p));}, error:function(e){console.log("oops " + JSON.stringify(e))}});
+//CONSOLE DEBUG STRING: pw.projects.getProjectEx({id:1,success:function(p){console.log(JSON.stringify(p));}, error:function(e){console.log("oops " + JSON.stringify(e))}});
+//TODO: $.extend() is copying functions all over the place. go back to testing if functions are defined and typeof == "function"
 
 pw.projects = (function(){
     var my = {};
@@ -22,8 +23,6 @@ pw.projects = (function(){
             date_created : ""
         }
 
-        //TODO: get properties getter/setters working
-
         this.getProperties = function(){
             return this.properties;
         }
@@ -37,14 +36,11 @@ pw.projects = (function(){
 
         //call this function to initialize the properties
         this.create = function(options, success, error){
-            options = $.extend({},_defaultOptions, options);
             $.extend(this.properties, options); //copy data into properties
             console.log("DEBUG: in create function of asset, properties is: {0}".format(JSON.stringify(this.properties)));
             //check for alterative success callback
             if(typeof success == "function"){
                 success(this);
-            }else if(typeof options.success == "function"){
-                options.success(this);
             }
             //no callback specified
         }
@@ -52,36 +48,62 @@ pw.projects = (function(){
         //function used to update a particular value in this asset
         //options argument expects: name (string), value (string) and optional success/error callbacks
         this.update = function(options){
-            options = $.extend({},_defaultOptions, options);
             var sql = "UPDATE assets SET {0} = '{1}' WHERE aid = {2}".format(options.name, options.value, properties.aid);
             pw.db.execute(sql, function(t,r){
                 //update self and hash object
                 properties[options.name] = options.value;
-                options.success(this);
+                if(typeof options.success == "function"){
+                    options.success(this);
+                }
             },function(t,e){
                 console.log("error when updating the argument value: {0}".format(JSON.stringify(e)));
-                options.error(e);
+                if(typeof options.error == "function"){
+                    options.error(e);
+                }
             });
         }
 
         this.remove = function(options){
-            options = $.extend({},_defaultOptions, options);
             var sqlDeleteFav = "DELETE FROM favorites WHERE aid={0}".format(properties.aid);
             var sql = "DELETE FROM assets WHERE aid={0}".format(properties.aid);
             pw.db.execute(sqlDeleteFav);
-            pw.db.execute(sql, options.success, options.error);
+            pw.db.execute(sql, function(){
+                if(typeof options.success == "function"){
+                    options.success();
+                }
+            }, function(){
+                if(typeof options.error == "function"){
+                    options.error();
+                }
+            });
         }
 
-        this.addFavorite = function(options){
-            options = $.extend({},_defaultOptions, options);
+        this.addFavorite = function(options, success, error){
+            //options = $.extend({},_defaultOptions, options);
             var sql = "INSERT INTO favorites VALUES({0}, {1})".format(properties.pid, properties.aid);
-            pw.db.execute(sql, options.success, options.error);
+            pw.db.execute(sql, function(t, r){
+                if(typeof success == "function"){
+                    success();
+                }
+            }, function(t, e){
+                if(typeof error == "function"){
+                    error(e);
+                }
+            });
         }
 
-        this.removeFavorite = function(options){
-            options = $.extend({},_defaultOptions, options);
+        this.removeFavorite = function(options, success, error){
+            //options = $.extend({},_defaultOptions, options);
             var sql = "DELETE FROM favorites WHERE pid={0} and aid={0})".format(properties.pid, properties.aid);
-            pw.db.execute(sql, options.success, options.error);
+            pw.db.execute(sql, function(t, r){
+                if(typeof success == "function"){
+                    success();
+                }
+            }, function(t, e){
+                if(typeof error == "function"){
+                    error(e);
+                }
+            });
         }
     }
 
@@ -107,24 +129,25 @@ pw.projects = (function(){
        }
 
        this.create = function(options, success, error){
-           options = $.extend({},_defaultOptions, options); //ensure success and error callbacks are defined
+           //options = $.extend({},_defaultOptions, options); //ensure success and error callbacks are defined
            console.log("DEBUG: in create function of project, options is: {0}".format(JSON.stringify(options)));
-           //$.extend(properties, options.data); //copy data into properties
-           var properties = this.setProperties(options);
-           properties.assets = new Array(); //clear out 'not_initialized' message in array
+           $.extend(this.properties, options); //copy data into properties
+           this.properties.assets = new Array(); //clear out 'not_initialized' message in array
            //get the assets for this project, assign to the internal array, then finally call supplied success callback
            var self = this;
            this.getAssets(options, function(assetsArray){
                if(typeof success == "function"){
                    self.properties.assets = assetsArray; //should use setProperties?
                    success(self);
-               }else{
-                   options.success(self); //pass our context to caller
+               }
+           }, function(e){
+               if(typeof error == "function"){
+                   error(e);
                }
            });
        }
 
-       this.addAsset = function(options){
+       this.addAsset = function(options, success, error){
            //copy options into defaults
            options = $.extend({path:"", success : function(){}, error : function(){}}, options);
            //regex to extract filename works for both \ and / file separators
@@ -138,22 +161,25 @@ pw.projects = (function(){
                var myAsset = new Asset();
                myAsset.create(options, function(newAsset){
                    this.properties.assets.push(newAsset); //add the new asset to our properties object
-                   options.success(newAsset);
+                   if(typeof success == "function"){
+                    success(newAsset);
+                   }
                });
            }, function(t,e){
-               options.error(e);
+               if(typeof error == "function"){
+                error(e);
+               }
            });
        }
 
        //on success returns an array of asset objects
-       //optionalCallback will be called instead of options.success if supplied as an argument
-       this.getAssets = function(options, optionalCallback){
-           //var properties = this.getProperties();
-           options = $.extend({},_defaultOptions, options); //copy default success & error callback functions into options
+       this.getAssets = function(options, success, error){
            var self = this;
            if(!this.properties.assets.length){ //check to see if the array already has something in it, run the query if it doesn't
-               if(!options.pid){
-                   options.error("id (project) must be specified when calling getAllAssets()");
+               if(!this.properties.pid){
+                   if(typeof error == "function"){
+                    error("pid must be specified when calling getAssets(). pid is: " + this.properties.pid);
+                   }
                    return false;
                }else{
                    var sql = "SELECT assets.aid, assets.pid, assets.path, assets.filename, assets.filetype, assets.date_created, favorites.aid as fav FROM assets LEFT OUTER JOIN favorites ON assets.aid = favorites.aid WHERE assets.pid = "+options.pid+" ORDER BY assets.date_created DESC";
@@ -172,61 +198,58 @@ pw.projects = (function(){
                            }
                        }
                        //check the optional callback to call it instead of what's in the options object
-                       if(typeof optionalCallback === "function"){
-                           optionalCallback(self.properties.assets);
-                       }else{
-                           options.success(self.properties.assets);
+                       if(typeof success == "function"){
+                           success(self.properties.assets);
                        }
                    }, function(t,e){
-                       options.error(e);
+                       if(typeof error == "function"){
+                        options.error(e);
+                       }
                    });
                }
-           }
-           //there was already something in the array, return what we have
-           if(typeof optionalCallback === "function"){
-               optionalCallback(this.properties.assets);
            }else{
-               options.success(this.properties.assets);
+               //there was already something in the array, return what we have
+               if(typeof success === "function"){
+                   success(this.properties.assets);
+               }
            }
+
        }
 
-       this.update = function(options){
+       this.update = function(options, success, error){
            console.log('DEBUG: update called on project {0} updating column {1} to {2}'.format(this.properties.id, options.name, options.value));
            var sql = "UPDATE projects SET {0} = '{1}' WHERE pid = {2}".format(options.name, options.value, this.properties.id);
            pw.db.execute(sql, function(t,r){
                //update self and hash object
                this.properties[options.name] = options.value;
-               if(typeof options.success == "function"){ //prevents the nasty error message if the function isn't passed
-                   options.success();
+               if(typeof success == "function"){ //prevents the nasty error message if the function isn't passed
+                   success();
                }
            },function(t,e){
                console.log("error when updating the project value: {0}".format(JSON.stringify(e)));
-               if(typeof options.error == "function"){ //prevents the nasty error message if the function isn't passed
-                   options.error(e);
+               if(typeof error == "function"){ //prevents the nasty error message if the function isn't passed
+                   error(e);
                }
            });
        }
 
-       this.remove = function(options){
+       this.remove = function(success, error){
            var sql = "DELETE FROM projects where pid={0}".format(this.properties.id);
            pw.db.execute(sql, function(t,r){
-               if(typeof options.success == "function"){ //prevents the nasty error message if the function isn't passed
-                   options.success(this);
+               if(typeof success == "function"){ //prevents the nasty error message if the function isn't passed
+                   success(this);
                }
            },function(t,e){
                console.log("error when deleting the project: {0}".format(JSON.stringify(e)));
-               if(typeof options.error == "function"){ //prevents the nasty error message if the function isn't passed
-                   options.error(e);
+               if(typeof error == "function"){ //prevents the nasty error message if the function isn't passed
+                   error(e);
                }
            });
        }
     }
 
-
-
     //options object expects an 'id' key corresponding to the id of the project to fetch
-    my.getProjectEx = function(options){
-        options = $.extend({},_defaultOptions, options);
+    my.getProjectEx = function(options, success, error){
         if(options.pid){
             var sql = "SELECT pid, name, description, active, date_created FROM projects WHERE pid = {0}".format(options.pid);
             //execute the database call to get the project
@@ -235,23 +258,21 @@ pw.projects = (function(){
                 if(r.rows.length){
                     //if we have results, create a new project object
                     var item = r.rows.item(0);
-                    //projectOptions contains the data from the database and the success/error callbacks
-                    var projectOptions = {
-                        data : item,
-                        success : options.success,
-                        error : options.error
-                    }
                     //want to copy the contents of the item to the options object
-                    options = $.extend({},item,options);//explanation: (options overrides item which overrides {})
+                    //options = $.extend({},item,options);//explanation: (options overrides item which overrides {})
                     //the init function will set all the property values and call the success/error callback as specified in the options argument
-                    myProject.create(options);
+                    myProject.create(item, function(project){
+                        success(project);
+                    }, error);
                 }
             }, function(t, e){
-                options.error(e);
+                if(typeof error == "function"){
+                    error(e);
+                }
             });
         }else{
             console.log("pid must be specified when calling getProject()");
-            options.error();
+            error("pid must be specified when calling getProject()");
         }
     }
 
